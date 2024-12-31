d9fffe30a2042408fa6d7141864bffc64fdb8aaf
Siftman <ssiftman@gmail.com>
2024-12-31 18:25:47 +0330

    fix

--- db.py
+++ db.py
@@ -5,6 +5,6 @@ qdrant_url = "http://localhost"
 client = QdrantClient(qdrant_url, port=qdrant_port, timeout=100.0)
 
 client.create_collection(
-    collection_name="your_company_name",
+    collection_name="dbstore",
     vectors_config=models.VectorParams(size=512, distance=models.Distance.COSINE),
 )
